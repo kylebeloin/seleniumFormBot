866f8703dd06587821260cfa1b4c0325efed7054
Kyle Beloin <kbeloin@email.arizona.edu>
2020-07-16 17:56:55 -0700

    Added Google API folder.

--- selenium_test/open_chrome.py
+++ selenium_test/open_chrome.py
@@ -1,14 +1,14 @@
 from selenium import webdriver
-from subprocess import call
+
 
 
 def openChrome():
-    chromedriver_location = "./chromedriver"
+    chromedriver_location = "./chromedriver" # Make sure the driver is in the same directory (or list the directory here.)
     driver = webdriver.Chrome(chromedriver_location)
     
-    driver.get('https://yuma.arizona.edu/request-info')
-    driver.implicitly_wait(3)
-    first_name = '//*[@id="form_913fa45e-b4ad-48d3-a26b-0e15918e3679"]'
+    driver.get('https://yuma.arizona.edu/request-info') # Opens the webpage
+    
+    first_name = '//*[@id="form_913fa45e-b4ad-48d3-a26b-0e15918e3679"]' # These are xpath elements. 
     last_name = '//*[@id="form_647beaf1-ccb7-4511-8a94-8bdadab42987"]'
     email = '//*[@id="form_31880a0a-62ae-4e68-961b-a79cb7b53e76"]'
     text_req = '//*[@id="form_9d8df43b-f59f-4bb4-85b6-8a22247c7c34_1"]'
@@ -18,13 +18,13 @@ def openChrome():
 
     submit = '//*[@id="form_7156f585-1060-4a2d-9bbb-3208433ebd56_container"]/div[3]/button'
 
-    first_gsheets = 'Kyle'
+    first_gsheets = 'Kyle' # Test value.
     #driver.find_element_by_xpath(first_name).click()
-    driver.implicitly_wait(3)
-    driver.find_element_by_xpath(first_name).send_keys(first_gsheets)
-    driver.find_element_by_xpath(submit).click()
-    driver.implicitly_wait(100)
-    while (True):
+    
+    driver.find_element_by_xpath(first_name).send_keys(first_gsheets) # sends test vallue to text box element.
+    driver.find_element_by_xpath(submit).click() # submit
+    
+    while (True): # this loop prevents chrome from closing after the driver is scrapped in the function. 
         pass
     #driver.find_element_by_xpath(last_name).click()
     #driver.find_element_by_xpath(email).click()
