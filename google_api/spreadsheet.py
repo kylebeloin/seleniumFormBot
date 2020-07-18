6695436dcc8a2c63739516c953a06fb83d29ecbb
Kyle Beloin <kbeloin@email.arizona.edu>
2020-07-18 15:48:58 -0700

    Commented spreadsheet.py; updated .gitignore file.

--- google_api/spreadsheet.py
+++ google_api/spreadsheet.py
@@ -1,11 +1,10 @@
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
 
-scope = ['https://www.googleapis.com/auth/spreadsheets']
+
 creds = gspread.service_account(filename='./client_secret.json')
 
 
-sheet = creds.open('yuma_sample').sheet1
+sheet = creds.open('yuma_sample').sheet1 # the .json and google sheet are a personal sample.
 
 data = sheet.get_all_records()
 
